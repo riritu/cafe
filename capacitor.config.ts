31ce58b3ad20b27e810773b62dde2f88a6b86429
renaf <eudaemii@gmail.com>
2023-07-17 22:52:44 +0800

    saphonenagana

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.trilc.gallery ',
+  appId: 'com.trilc.gallery',
   appName: 'TRILC U: Book and Tenant Management',
   webDir: 'www',
   server: {
